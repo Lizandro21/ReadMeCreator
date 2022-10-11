11f6961f3adc7eaa3f8b4f51e4deb2c31a6c615a
Lizandro <businesslizandro@gmail.com>
2022-10-11 16:30:55 -0500

    Pushing the ReadME creator

---
+++ index.js
@@ -0,0 +1,96 @@
+//deoendecies
+const inquirer = require('inquirer')
+const fs = require('fs')
+//heres the readme questions
+const readmeQuestions = () =>
+    inquirer.prompt([
+    {
+        type: "input",
+        message: "What's the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "A short description of your project.",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "Select any licenses that you need/used in the project. If any.",
+        choices: ["ISC", 'Mozilla', "MIT", "Creative Commons license family", "None"],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "Are there any instructions to install dependencies?",
+        name: "installations",
+    },
+    {
+        type: "input",
+        message: "Are there any special instructions for your user to be able to use this project?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Are there any tests that need to run and how to use them?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Who is/are the creator(s) of this project?",
+        name: "creators",
+    },
+    {
+        type: "input",
+        message: "What's your/teams Github username(s)?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What's your/teams email-adress?",
+        name: "email",
+    },
+]);
+//heres the function that creates the actualy read me document
+function readmeCreator(answers){
+    //readme being created
+return`# ${answers.title}
+
+## Table of Contents:
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Creators](#creators)
+
+## Description:
+${answers.description}
+
+## Installation:
+In order to run this project follows these instructions please!:
+\`\`\`${answers.installations}\`\`\`
+
+## Usage:
+${answers.usage}
+
+## License:
+ ${answers.license}
+
+## Tests:
+${answers.tests}
+
+## Creators:
+*Feel free to contact me for any questions!
+*Name(s): ${answers.creators}
+*Email(s): ${answers.email}
+*Github: [GitHub](https://github.com/${answers.username})
+`
+}
+//herse the readme all being made with the functions being called
+readmeQuestions()
+.then((answers) => {
+fs.writeFile(`README.md`, readmeCreator(answers),
+    (err) => {
+        err ? console.error(err) : console.log('ReadME created succesfully, YAY!')
+    })}
+);
